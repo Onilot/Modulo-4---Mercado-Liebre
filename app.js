b3394ce5a4cae3214497044b54bcfccc29461c05
Onilot <onilotlr@gmail.com>
2022-10-19 16:02:54 -0300

    Primer commit
    Subiendo al repositorio remoto el proyecto mercado liebre

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicFolderPath = path.resolve('public');
+app.use(express.static(publicFolderPath));
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log('Servidor funcionando en puerto $ {port}')
+});
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/views/home.html')
+});
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/register.html'))
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/login.html'))
+})
+app.get('/offers', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/offers.html'))
+})
